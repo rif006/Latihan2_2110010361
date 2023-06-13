0fa8bac679d5b6a3a4673929fbb4f4dce66e41e7
rif006 <rif.006@gmail.com>
2023-06-13 20:03:26 +0800

    Melengkapi Class Mahasiswa

--- src/universitas/Mahasiswa.java
+++ src/universitas/Mahasiswa.java
@@ -7,8 +7,45 @@ public class Mahasiswa {
     private String nama;
     private String prodi;
     
-}
+    public Mahasiswa(String npm,String nama, String prodi){
+        this.npm = npm;
+        this.nama = nama;
+        this.prodi = prodi;
+        
+    }
 
-public String getDetail(){
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProdi() {
+        return prodi;
+    }
+
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    
+    
+    public String getDetail(){
     
 }
+    
+}
+
+
+
+
